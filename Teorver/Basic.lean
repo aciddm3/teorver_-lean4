0a4a930a6827a30490c366600b746845aead4545
aciddm3 <dmacid3@gmail.com>
2026-04-17 02:20:44 +0700

    2

--- Teorver/Basic.lean
+++ Teorver/Basic.lean
@@ -8,6 +8,8 @@ import Mathlib.Data.Fintype.Basic
   -- конечные типы, Fintype.card
 import Mathlib.Data.Finset.Basic
   -- конечные подмножества, Finset.filter, Finset.image
+import Mathlib.Order.Heyting.Basic
+
 import Mathlib.Tactic.Linarith
   -- автоматизация линейных неравенств
 import Mathlib.Tactic.FieldSimp
@@ -16,6 +18,7 @@ import Mathlib.Topology.Algebra.InfiniteSum.Basic
 
 import Teorver.SigmaAlgebra
 
+#find "∑' n, ?f n = ?f 0 + ∑' n, ?f (n + 1)"
 
 structure ProbSpace (Ω : Type*) where
   F : SigmaAlgebra Ω
@@ -23,31 +26,128 @@ structure ProbSpace (Ω : Type*) where
   p_nonneg : ∀ s ∈ F.sets, 0 ≤ p s      -- 2. Неотрицательность
   p_univ : p Set.univ = 1               -- 3. Нормировка на полном множестве
   p_add : ∀ (f : Nat → Set Ω),          -- 4. σ-аддитивность
-    (∀ n, f n ∈ F.sets) →
-    (∀ i j, i ≠ j → Disjoint (f i) (f j)) →
+    (∀ n, f n ∈ F.sets) ->
+    (∀ i j, i ≠ j -> Disjoint (f i) (f j)) ->
     p (⋃ n, f n) = ∑' n, p (f n)
+namespace ProbSpace
 
+theorem prob_empty_eq_zero {α : Type*} (P : ProbSpace α) : P.p ∅ = 0 := by
+  let empty_seq : Nat -> Set α := fun n ↦ ∅
+  have hav1 : P.p (⋃ n, empty_seq n) = ∑' n, P.p (empty_seq n) := by
+    have empty_seq_in_f
+      : ∀ n, empty_seq n ∈ P.F.sets := by
+      simp [empty_seq]
+      apply SigmaAlgebra.empty_mem
+    have disj
+      : ∀ i j, i ≠ j -> Disjoint (empty_seq i) (empty_seq j) := by
+      intros i j _
+      simp [empty_seq]
+    apply P.p_add empty_seq empty_seq_in_f disj
+  simp [empty_seq] at hav1
+  exact hav1
 
-open Topology
+lemma p_add2 {α : Type*} (P : ProbSpace α) {A B : Set α}
+    (hA : A ∈ P.F.sets) (hB : B ∈ P.F.sets) (hD : Disjoint A B)
+    : P.p (A ∪ B) = P.p A + P.p B := by
+  let seq : ℕ → Set α := fun n =>
+      if n = 0 then A
+      else if n = 1 then B
+      else ∅
+  have hav_add : P.p (⋃ n, seq n) = ∑' n, P.p (seq n) := by
+      have hav2_1 :  ∀ n, seq n ∈ P.F.sets := by
+        intro n
+        by_cases h0 : n = 0
+        .
+          rw[h0]
+          simp [seq]
+          exact hA
+        .
+          by_cases h1 : n = 1
+          .
+            rw[h1]
+            simp [seq]
+            exact hB
+          .
+            simp [seq, h0, h1]
+            exact SigmaAlgebra.empty_mem P.F
+      have hav2_2
+        : ∀ i j, i ≠ j → Disjoint (seq i) (seq j) := by
+          intro n1 n2 h
+          rcases n1 with _|_|_
+          · rcases n2 with _|_|_
+            . contradiction
+            . simp [seq]
+              apply hD
+            . simp [seq, Disjoint]
+          · rcases n2 with _|_|_
+            . simp [seq]
+              apply hD.symm
+            . contradiction
+            . simp [seq, Disjoint]
+          · rcases n2 with _|_|_
+            . simp [seq]
+            . simp [seq]
+            . simp [seq, Disjoint]
+      exact P.p_add seq hav2_1 hav2_2
+  simp at hav_add
+  have h_sum_split : ∑' n, P.p (seq n) = P.p (seq 0) + P.p (seq 1) + ∑' n, P.p (seq (n+2)) := by
+    rw [tsum_eq_sum (s := Finset.range 2)]
+    simp [Finset.sum_range_succ]
+    simp [seq]
+    intro h
+    simp [seq]
+    intro h2
+    cases h with
+    | zero => contradiction          -- 1 < 0 → противоречие
+    | succ h' => cases h' with
+      | zero => contradiction        -- 1 < 1 → противоречие
+      | succ _ => simp [prob_empty_eq_zero P]
+  rw [<- hav_add] at h_sum_split
+  rw [<- Set.union_iUnion_nat_succ seq] at h_sum_split
+  rw [<- Set.union_iUnion_nat_succ fun n => seq (n+1)] at h_sum_split
+  simp [seq] at h_sum_split
+  exact h_sum_split
 
-theorem p_empty_eq_zero {T : Type*} (P : ProbSpace T) : P.p ∅ = 0 := by
-  -- 1. Применяем счётную аддитивность к последовательности ∅
-  have h := P.p_add (fun _ => ∅)
-    (fun _ => sigma_algebra_empty_mem P.F)
-    (by simp [Disjoint])
-  -- h : P.p (⋃ n, ∅) = ∑' n, P.p ∅
+theorem prob_of_opposite {α : Type*} (P : ProbSpace α) : ∀ A ∈ P.F.sets, P.p (Aᶜ) = 1 - P.p A := by
+  intros A h
+  have hs : P.p Set.univ = P.p Aᶜ + P.p A := by
+    have disj : Disjoint Aᶜ A := disjoint_compl_left
+    have sigma : Aᶜ ∈ P.F.sets := P.F.compl_mem A h
+    let add2 := p_add2 P sigma h disj
+    simp at add2
+    exact add2
+  rw [P.p_univ] at hs
+  apply_fun fun x => x - P.p A at hs
+  simp at hs
+  exact hs.symm
 
-  -- 2. ⋃ n, ∅ = ∅
-  simp [Set.iUnion_const] at h
-  -- h : P.p ∅ = ∑' n, P.p ∅
+theorem prob_mono {α : Type*} (P : ProbSpace α) {A B : Set α}
+    (hA : A ∈ P.F.sets) (hB : B ∈ P.F.sets) (hAB : A ⊆ B)
+    : P.p A ≤ P.p B := by
+    have hB_eq : B = A ∪ (B \ A) := by
+      simp [Set.union_diff_self]
+      rw [Eq.comm]
+      rwa [Set.union_eq_right]
+    rw [hB_eq]
 
-  -- 3. Разбиваем сумму: ∑' f n = f 0 + ∑' f (n+1)
-  have h_shift : ∑' n : ℕ, P.p ∅ = P.p ∅ + ∑' n : ℕ, P.p ∅ := by
-    rw [tsum_nat_eq 1]           -- стабильная лемма в Mathlib4
-    simp [Finset.sum_range_one]  -- ∑_{i∈{0}} f i = f 0
-    congr! 1                     -- доказываем f (n+1) ≡ f n
-    ext n; simp
+    have h_diff : B \ A ∈ P.F.sets := by
+      rw [Set.diff_eq]
+      exact
+        SigmaAlgebra.intersect2_mem P.F hB
+        (SigmaAlgebra.compl_mem P.F A hA)
 
-  -- 4. Подставляем и решаем x = x + x ⇒ x = 0
-  rw [h_shift] at h
-  linarith
+    have disj : Disjoint A (B \ A) := by
+      rw [Set.disjoint_iff_inter_eq_empty]
+      simp[Set.diff_eq]
+      rw [<- Set.inter_comm]
+      rw [Set.inter_assoc]
+      simp
+
+    rw [p_add2 P hA h_diff disj]
+    have h_nonneg : 0 ≤ P.p (B \ A) := P.p_nonneg (B \ A) h_diff
+    linarith [h_nonneg]
+
+theorem prob_max {α : Type*} {P : ProbSpace α} {A : Set α}
+  (hA : A ∈ P.F.sets) : P.p A ≤ 1 := by
+  rw [<- P.p_univ]
+  apply prob_mono P hA P.F.univ_mem (Set.subset_univ A)
--- Teorver/SigmaAlgebra.lean
+++ Teorver/SigmaAlgebra.lean
@@ -8,15 +8,18 @@ structure SigmaAlgebra (Ω : Type*) where
   sets : Set (Set Ω)
   univ_mem : Set.univ ∈ sets
   compl_mem : ∀ s ∈ sets, sᶜ ∈ sets
-  iUnion_mem : ∀ s : Nat → Set Ω, (∀ i, s i ∈ sets) → (⋃ i, s i) ∈ sets
+  iUnion_mem : ∀ s : Nat -> Set Ω, (∀ i, s i ∈ sets) -> (⋃ i, s i) ∈ sets
 
-theorem sigma_algebra_empty_mem {T : Type*} (F : SigmaAlgebra T) : ∅ ∈ F.sets := by
+namespace SigmaAlgebra
+
+theorem empty_mem {T : Type*} (F : SigmaAlgebra T)
+: ∅ ∈ F.sets := by
   have hav1 : Set.univ ∈ F.sets := by exact F.univ_mem
   have h_compl : Set.univᶜ ∈ F.sets := by exact F.compl_mem Set.univ hav1
   rw [Set.compl_univ] at h_compl
   exact h_compl
 
-theorem sigma_algebra_iintersect_mem {T : Type*} (F : SigmaAlgebra T) :
+theorem iintersect_mem {T : Type*} (F : SigmaAlgebra T) :
   ∀ s : Nat → Set T, (∀ i, s i ∈ F.sets) → (⋂i, s i) ∈ F.sets := by
   intro s h_s
   have h_compl : ∀ i, (s i)ᶜ ∈ F.sets := by
@@ -27,8 +30,28 @@ theorem sigma_algebra_iintersect_mem {T : Type*} (F : SigmaAlgebra T) :
   have h_compl_union : (⋃ i, (s i)ᶜ)ᶜ ∈ F.sets :=
     F.compl_mem _ h_union
   rw [Set.compl_iUnion] at h_compl_union
-  simp at h_compl_union  -- убирает двойное дополнение ((s i)ᶜ)ᶜ → s i
+  simp at h_compl_union -- убирает двойное дополнение ((s i)ᶜ)ᶜ → s i
   exact h_compl_union
+
+theorem intersect2_mem {T : Type*} (F : SigmaAlgebra T) {A B : Set T}
+  : (A ∈ F.sets) -> (B ∈ F.sets) -> (A ∩ B ∈ F.sets) := by
+    intro hA hB
+    let seq n := if n=0 then A else if n=1 then B else Set.univ
+    have seq_mem : (⋂i, seq i) ∈ F.sets := by
+      have hav1 : ∀ i, seq i ∈ F.sets := by
+        intro n
+        rcases n with _|_|_
+        . simp[seq]
+          exact hA
+        . simp[seq]
+          exact hB
+        . simp[seq]
+          exact F.univ_mem
+      apply F.iintersect_mem seq hav1
+    rw [<-Set.inter_iInter_nat_succ,
+      <-Set.inter_iInter_nat_succ] at seq_mem
+    simp [seq] at seq_mem
+    exact seq_mem
 
 def PowerSetSigmaAlgebra (Ω : Type*) : SigmaAlgebra Ω where
 -- сигма алгебра - множество всех подмножеств
